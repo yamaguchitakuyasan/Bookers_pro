99de5127e21df923d15184bc2fe2b170d049c321
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-18 22:14:10 +0900

    books where

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
 
 	def create
     	@book = Book.new(book_params)
+        @book.user_id = current_user.id
     	@book.save
     	redirect_to book_path(@book.id)
     end
