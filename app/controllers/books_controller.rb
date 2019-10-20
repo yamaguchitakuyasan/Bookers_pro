6a934ae86892170953561f1ca36e4f1cf5607c1a
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-20 20:53:15 +0900

    changing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
     	book = Book.new(book_params)
         book.user_id = current_user.id
     	book.save
-    	redirect_to book_path
+    	redirect_to book_path(book.id)
     end
 
     def update
